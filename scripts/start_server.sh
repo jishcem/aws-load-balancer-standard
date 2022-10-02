bf0013144b355514437c641a9848252e311d1f20
Ajeesh Joshy <jishcem@gmail.com>
2022-10-02 14:07:40 +0530

    Update codedeploy scripts

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,3 +3,4 @@ echo "Starting the server"
 sudo npm install --prefix /home/ubuntu/Code/express-codedeploy-3
 npm run build --prefix /home/ubuntu/Code/express-codedeploy-3
 sudo systemctl restart node-api.service
+sudo systemctl restart nginx
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -2,5 +2,3 @@ echo "Stopping the server"
 
 systemctl stop node-api.service
 sudo rm -rf /home/ubuntu/Code/express-codedeploy-3
-
-systemctl restart nginx
