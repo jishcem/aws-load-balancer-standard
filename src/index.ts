280d0dbbda6234e0e2d45f730ff52d6797b3ec7b
Ajeesh Joshy <jishcem@gmail.com>
2022-10-05 08:26:19 +0530

    Add code to get ip address

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from 'express'
+import { networkInterfaces } from 'os'
 
 const app = express()
 const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
-    res.send("Hello World, I want to be a good devops guy")
+    const nets = networkInterfaces();
+    const results = Object.create(null);
+    for (const name of Object.keys(nets)) {
+        const network = nets[name];
+        if (network !== undefined) {
+            for (const net of network) {
+                const familyV4Value = typeof net.family === 'string' ? 'IPv4' : 4
+                if (net.family === familyV4Value && !net.internal) {
+                    if (!results[name]) {
+                        results[name] = [];
+                    }
+                    results[name].push(net.address);
+                }
+            }
+        }        
+    }
+
+    res.send(`Hello World, aws load balancer, the ip address is ${JSON.stringify(results)}`)
 })
 
 app.get('/health', (req: Request, res: Response) => {
